1efceb35ad69b570570477832891598bec67e540
Jasmine <jazzykitkat@windowslive.com>
2019-10-01 19:06:03 +1300

    MainMenu And Pause Menu

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+
+    public Button playButton;
+    public Button quitButton;
+
+    void OnClickPlay()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void OnClickQuit()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+}
---
+++ Assets/Scripts/PauseMenuUI.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuUI : MonoBehaviour
+{
+    public Button resumeButton;
+    public Button quitButton;
+
+    bool isPaused;
+
+    public void OnClickResume()
+    {
+        onPaused();
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            onPaused();
+        }    
+    }
+
+    public void showPaused()
+    {
+
+    }
+
+    public void hidePaused()
+    {
+
+    }
+
+    public void onPaused()
+    {       
+        if (isPaused == false)
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+            Debug.Log("IsPaused");
+            //showPaused();
+        }
+        else if (isPaused == true)
+        {
+            Time.timeScale = 1;
+            Debug.Log("IsResumed");
+            isPaused = false;
+            //hidePaused();
+        }
+    }
+}
