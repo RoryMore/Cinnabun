bd4d9d4afeb325b20284b6faff902911bb255729
Rory <ruairidh.more@gmail.com>
2019-10-02 13:54:05 +1300

    Basic entity version #1 complete

---
+++ Assets/Scripts/SimpleEnemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleEnemy : Entity
+{
+
+    public float meleeAttackRange;
+    NavMeshAgent nav;
+
+    Transform target;
+
+    bool isAttacking = false;
+
+    void Awake()
+    {
+
+        target = GameObject.Find("Player").transform;
+
+        nav = GetComponent<NavMeshAgent>();
+
+
+
+
+
+    }
+
+
+    void Update()
+    {
+        Movement();
+
+
+    }
+
+
+    public void Movement()
+    {
+        nav.SetDestination(target.transform.position);
+    }
+
+    //TEMPORARY FUNCTION FOR WHEN JASMINE FINISHES HER TURN COUNTER
+
+
+
+}
+
