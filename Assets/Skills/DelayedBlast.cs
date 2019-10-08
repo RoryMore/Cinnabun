8f0f0d6eed1a8e34de882d31263f71119fd381c8
Rory <ruairidh.more@gmail.com>
2019-10-08 15:47:59 +1300

    Delayed blast 80% complete, teleport 100% complete

--- Assets/Skills/DelayedBlast.cs
+++ Assets/Skills/DelayedBlast.cs
@@ -6,30 +6,106 @@ using UnityEngine;
 
 public class DelayedBlast : SkillData
 {
-    // Start is called before the first frame update
-    //public override void CastSkill(Transform zoneStart, SkillShape shape)
-    //{
-    //    DrawRangeIndicator(zoneStart, shape);
+    Entity entityTarget1 = null;
+    int numOfDelayedBlasts = 0;
 
-    //    float drawPercent = (timeSpentOnWindUp / windUp);
-    //    rangeIndicator.DrawCastTimeIndicator(zoneStart, angleWidth, 0.0f, range, drawPercent);
-    //    //rectangleRangeIndicator.DrawCastTimeIndicator(zoneStart, angle, 0.0f, drawPercent, range);
+    public override void TargetSkill(Transform zoneStart)
+    {
+        // Entity is not set; therefore we need to wait until the user has set the entity
+        if (entityTarget1 == null)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, 400))
+            {
+                Vector3 lookAt = new Vector3(hit.point.x, zoneStart.position.y, hit.point.z);
+                zoneStart.LookAt(lookAt);
+            }
 
-    //    // Increment the time spent winding up the skill
-    //    timeSpentOnWindUp += Time.deltaTime;
+            // We are drawing the range indicator here so the player knows if what they are clicking is in range
+            // If being in range is relevant
+            DrawRangeIndicator(zoneStart, shape);
 
-    //    // When the skill can be activated
-    //    if (timeSpentOnWindUp >= windUp)
-    //    {
-    //        ActivateSkill();
-    //        timeSpentOnWindUp = 0.0f;
-    //        currentlyCasting = false;
-    //    }
-    //}
+            // Select our entity target
+            SelectTargetRay(zoneStart, ref entityTarget1, true);
+            // The true value in the SelectTargetRay function is specifying that we want to also make a check
+            // to see if the target is in range
+            // We can leave that extra field blank, or false, if we don't want to make that check
+        }
+        else if (entityTarget1 != null)
+        {
+            // Start casting the skill
+            CastSkill(zoneStart);
+        }
 
-    void ActivateSkill()
+    }
+
+    protected override void CastSkill(Transform zoneStart)
+    {
+        currentlyCasting = true;
+
+        DrawRangeIndicator(zoneStart, shape);
+
+        float drawPercent = (timeSpentOnWindUp / windUp);
+        rangeIndicator.DrawCastTimeIndicator(zoneStart, angleWidth, 0.0f, range, drawPercent);
+
+        // Increment the time spent winding up the skill
+        timeSpentOnWindUp += Time.deltaTime;
+
+        // When the skill can be activated
+        if (timeSpentOnWindUp >= windUp)
+        {
+            currentlyCasting = false;
+            ActivateSkill();
+            timeSpentOnWindUp = 0.0f;
+        }
+    }
+
+    protected override void ActivateSkill()
     {
         timeBeenOnCooldown = 0.0f;
-        // What happens when the skill is activated
+        numOfDelayedBlasts = 0;
+
+        //Assuming the list is empty, give it a delayed blast condition
+        if (entityTarget1.ReturnConditions().Count == 0)
+        {
+            Entity.Condition delayedBlast = new Entity.Condition();
+            delayedBlast.conditionType = Entity.ConditionType.DELAYEDBLAST;
+            delayedBlast.duration = 9.0f;
+            entityTarget1.currentConditions.Add(delayedBlast);
+            Debug.Log("Tick tick tick...");
+            entityTarget1 = null;
+
+
+
+        }
+        else
+        {
+
+            for (int i = 0; i < entityTarget1.currentConditions.Count; i++)
+            {
+                if (entityTarget1.currentConditions[i].conditionType == Entity.ConditionType.DELAYEDBLAST)
+                {
+                    entityTarget1.currentConditions.Remove(entityTarget1.currentConditions[i]);
+                    entityTarget1.TakeDamage(baseDamage);
+                    numOfDelayedBlasts++;
+                    Debug.Log("BOOM!");
+                    entityTarget1 = null;
+                    break;
+                }
+                
+            }
+            
+
+            if (numOfDelayedBlasts == 0)
+            {
+            Entity.Condition delayedBlast = new Entity.Condition();
+            delayedBlast.conditionType = Entity.ConditionType.DELAYEDBLAST;
+            delayedBlast.duration = 9.0f;
+            entityTarget1.currentConditions.Add(delayedBlast);
+            Debug.Log("Tick tick tick...");
+            entityTarget1 = null;
+            }
+
+        }
     }
 }
