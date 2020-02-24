5f60f62c715933fb44bf4993ed70a99593730002
Jasmine <jazzykitkat@windowslive.com>
2020-02-25 10:53:52 +1300

    timesincesartup

---
+++ Assets/TimeSinceStartUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeSinceStartUp : MonoBehaviour
+{
+    [SerializeField] private Transform damageNumbers;
+    private TextMeshPro textNumbers;
+    float timeNumbers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textNumbers = transform.GetComponent<TextMeshPro>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeNumbers = Time.timeSinceLevelLoad;
+
+        textNumbers.SetText(timeNumbers.ToString());
+    }
+}
