239df5807855d9b82e176963dc3f762146313040
Jasmine <jazzykitkat@windowslive.com>
2020-02-25 13:30:05 +1300

    pause screen vhs stuff

--- Assets/TimeSinceStartUp.cs
+++ Assets/TimeSinceStartUp.cs
@@ -2,24 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class TimeSinceStartUp : MonoBehaviour
 {
-    [SerializeField] private Transform damageNumbers;
-    private TextMeshPro textNumbers;
+
+    PauseAbility pause;
+    private Text textNumbers;
     float timeNumbers;
+    Color textColor;
     // Start is called before the first frame update
     void Start()
     {
-        textNumbers = transform.GetComponent<TextMeshPro>();
-       
+        textNumbers = transform.GetComponent<Text>();
+        pause = FindObjectOfType<PauseAbility>();
+       // textColor = textNumbers;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeNumbers = Time.timeSinceLevelLoad;
 
-        textNumbers.SetText(timeNumbers.ToString());
+         
+        
+      
+
+        timeNumbers = Time.timeSinceLevelLoad;
+        textNumbers.text = timeNumbers.ToString();
+
+         if (pause.takeingTurn == true)
+          {
+            textColor.a = 1.0f;
+        }
+       else if (pause.takeingTurn == false)
+     {
+            textColor.a = 0.0f;
+        }
+
+        textNumbers.color = textColor;
     }
 }
