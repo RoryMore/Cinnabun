5d9322210ae51ffb9002e1a7ecb1ba1b75af2ded
Sunny Peebles <sunny.peebles@gmail.com>
2020-02-27 23:51:36 +1300

    Drops will now always spawn above/on the NavMesh. +BloodOrbDropControl
    * Cleaned up debug logs.
    * Drops now don't have the possibility of being dropped inside nearby terrain/scenery objects
    * Improved player picking up items (equipment) from ground interaction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -329,14 +329,15 @@ public class Player : Entity
 
             if (Physics.Raycast(ray, out RaycastHit hit, moveRaycastDistance, groundLayerMask))
             {
-                //if (hit.collider.tag.Contains("Finish"))
-                //{
-                nav.SetDestination(hit.point);
-
-                Debug.Log("Move Player");
-                //}
+                if (hit.collider.tag.Contains("Item"))
+                {
+                    nav.SetDestination(hit.collider.transform.position);
+                }
+                else
+                {
+                    nav.SetDestination(hit.point);
+                }
             }
-
         }
     }
 
